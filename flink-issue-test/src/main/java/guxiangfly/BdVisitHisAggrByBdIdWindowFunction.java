05aa836310befa80016ef845099c2d3e29c561df
guxiangfly <guxiang02@meituan.com>
2021-06-23 13:24:29 +0800

    feature: 测试进行相应修改

--- flink-issue-test/src/main/java/guxiangfly/BdVisitHisAggrByBdIdWindowFunction.java
+++ flink-issue-test/src/main/java/guxiangfly/BdVisitHisAggrByBdIdWindowFunction.java
@@ -8,7 +8,6 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.windowing.RichWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.streaming.runtime.operators.windowing.WindowOperator;
 import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
